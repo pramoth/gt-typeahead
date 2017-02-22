c6296c183c93b74f5f36e3d8825d49230831bc1a
Saranrat <saranrat@geniustree.co.th>
2017-02-22 14:40:24 +0700

    fix show value ngmodel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
-  ctrLocation:any;
+  ctrLocation:any={
+    version: 23,
+    createDate: "2003-01-05",
+    createBy: "develop",
+    updateDate: "2014-06-10",
+    updateBy: "administrator",
+    locId: 1,
+    locCode: "100000",
+    regId: 1,
+    locAbbrName: "กรุงเทพมหานคร",
+    locName: "กรุงเทพมหานคร",
+    locRepName: "กรุงเทพมหานคร",
+    aumpCode: "100000",
+    provCode: "100000",
+    postcode: null,
+    provName: "กรุงเทพมหานคร",
+    aumpName: null,
+    tumbonName: null,
+    useFlag: "1"
+  };
   select(value:any){
     console.log(value)
   }
+
+
 }
--- src/app/typeahead/gt-typeahead/gt-typeahead.component.ts
+++ src/app/typeahead/gt-typeahead/gt-typeahead.component.ts
@@ -11,112 +11,113 @@ import  "rxjs/add/operator/scan";
 import {ControlValueAccessor, NG_VALUE_ACCESSOR, FormControl, NG_VALIDATORS} from "@angular/forms";
 
 export function createRequiredValidator(name: string) {
-    return function requiredValidator(c: FormControl) {
-        return c.value ? null : `${name} is required`;
-    }
+  return function requiredValidator(c: FormControl) {
+    return c.value ? null : `${name} is required`;
+  }
 }
 
 @Component({
-    selector: 'gt-typeahead',
-    templateUrl: './gt-typeahead.component.html',
-    styleUrls: ['./gt-typeahead.component.css'],
-    providers: [
-        {
-            provide: NG_VALUE_ACCESSOR,
-            useExisting: forwardRef(() => GtTypeaheadComponent),
-            multi: true
-        },
-        {
-            provide: NG_VALIDATORS,
-            useValue: forwardRef(() => GtTypeaheadComponent),
-            multi: true
-        }
-    ]
+  selector: 'gt-typeahead',
+  templateUrl: './gt-typeahead.component.html',
+  styleUrls: ['./gt-typeahead.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => GtTypeaheadComponent),
+      multi: true
+    },
+    {
+      provide: NG_VALIDATORS,
+      useValue: forwardRef(() => GtTypeaheadComponent),
+      multi: true
+    }
+  ]
 })
 export class GtTypeaheadComponent implements OnInit,ControlValueAccessor,OnChanges {
 
-    private value: any;
-    private term$ = new Subject<string>();
-    @Input()
-    private variable: string;
-    @Input()
-    private url: string;
-    @Input()
-    private displayProp: string;
-    @Input()
-    private required: boolean;
-    @Input()
-    private name: string;
-    @Input()
-    private disabled: boolean;
+  private value: any;
+  private term$ = new Subject<string>();
+  @Input()
+  private variable: string;
+  @Input()
+  private url: string;
+  @Input()
+  private displayProp: string;
+  @Input()
+  private required: boolean;
+  @Input()
+  private name: string;
+  @Input()
+  private disabled: boolean;
 
-    private data;
-    private text;
-    private propagateChangeFn: any;
-    private validateFn: Function;
-    @Output()
-    private selected = new EventEmitter < any >();
-    @Output("clear")
-    private clearEvent = new EventEmitter < any >();
+  private data;
+  private text;
+  private propagateChangeFn: any;
+  private validateFn: Function;
+  @Output()
+  private selected = new EventEmitter < any >();
+  @Output("clear")
+  private clearEvent = new EventEmitter < any >();
 
-    constructor(private http: Http) {
+  constructor(private http: Http) {
+  }
+
+  ngOnInit() {
+    this.term$
+      .debounceTime(300)
+      .distinctUntilChanged()
+      .switchMap(term => this.search(term))
+      .map(e => e.json())
+      .subscribe(e => this.data = e);
+    console.log(this.value)
+  }
+
+  search(term: string) {
+    return this.http.get(`${this.url}?${this.variable}=${term}`)
+  }
+
+  select(value: any) {
+    this.setValue(value);
+    this.selected.emit(value);
+    this.propagateChangeFn(this.value);
+  }
+
+  setValue(value: any) {
+    this.value = value;
+    this.data = null;
+    this.text = value[this.displayProp];
+  }
+
+  clear() {
+    this.value = null;
+    this.data = null;
+    this.text = null;
+    this.propagateChangeFn(this.value);
+    this.clearEvent.emit(this.value);
+  }
+
+  writeValue(value: any): void {
+    if (value) {
+      this.setValue(value);
     }
+  }
 
-    ngOnInit() {
-        this.term$
-            .debounceTime(300)
-            .distinctUntilChanged()
-            .switchMap(term => this.search(term))
-            .map(e => e.json())
-            .subscribe(e => this.data = e);
-    }
+  registerOnChange(fn: any): void {
+    this.propagateChangeFn = fn;
+  }
 
-    search(term: string) {
-        return this.http.get(`${this.url}?${this.variable}=${term}`)
-    }
+  registerOnTouched(fn: any): void {
+  }
 
-    select(value: any) {
-        this.setValue(value);
-        this.selected.emit(value);
-        this.propagateChangeFn(this.value);
+  ngOnChanges(changes: any): void {
+    if (changes.required && this.required) {
+      this.validateFn = createRequiredValidator(this.name);
     }
+  }
 
-    setValue(value: any) {
-        this.value = value;
-        this.data = null;
-        this.text = value[this.displayProp];
-    }
-
-    clear() {
-        this.value = null;
-        this.data = null;
-        this.text = null;
-        this.propagateChangeFn(this.value);
-        this.clearEvent.emit(this.value);
-    }
-
-    writeValue(value: any): void {
-        if (value !== undefined) {
-            this.value = value;
-        }
-    }
-
-    registerOnChange(fn: any): void {
-        this.propagateChangeFn = fn;
-    }
-
-    registerOnTouched(fn: any): void {
-    }
-
-    ngOnChanges(changes: any): void {
-        if (changes.required && this.required) {
-            this.validateFn = createRequiredValidator(this.name);
-        }
-    }
-
-    validate(c: FormControl) {
-        return this.validateFn(c);
-    }
+  validate(c: FormControl) {
+    return this.validateFn(c);
+  }
 
 }
 
